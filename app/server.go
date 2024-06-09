e4e8688f567c0e71d45d2f867e6c15d012cd7f54
Snir Koppelman <snirkwork@gmail.com>
2024-06-09 22:51:34 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -7,19 +7,49 @@ import (
 	"os"
 )
 
+// Only supported version
+const HTTPVersion = "HTTP/1.1"
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+	if err := run(); err != nil {
+		fmt.Println("run error:", err)
 		os.Exit(1)
 	}
 }
+
+func run() error {
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		return fmt.Errorf("failed to bind to port 4221: %w", err)
+	}
+	defer func() { _ = l.Close() }()
+
+	// TODO: handle signals, then loop on connections
+	conn, err := l.Accept()
+	if err != nil {
+		return fmt.Errorf("error accepting connection: %w", err)
+	}
+	handleConn(conn)
+
+	return nil
+}
+
+func handleConn(conn net.Conn) {
+	err := respond(conn)
+	if err != nil {
+		fmt.Println("ERROR:,", err)
+	}
+	conn.Close()
+}
+
+func respond(conn net.Conn) error {
+	_, err := conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	return err
+}
+
+// func parseRequest(data []byte) error {
+//     // Split by end markers
+//     bytes.Split()
+// }
